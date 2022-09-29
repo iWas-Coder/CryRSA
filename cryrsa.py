7b2d5e00f199376d0b0e53258ca32a9f6b89ea0c
Wasym Atieh Alonso <72618577+iWas-Coder@users.noreply.github.com>
2022-09-29 12:13:23 +0200

    Create cryrsa.py

---
+++ cryrsa.py
@@ -0,0 +1,18 @@
+import modules.args as args
+import modules.funcs as funcs
+
+
+# === MAIN === #
+def main():
+    """
+    Main program flow.
+    """
+    
+    # Arguments
+    args.init_args()
+    
+    pass
+
+
+if __name__ == "__main__":
+    main()
